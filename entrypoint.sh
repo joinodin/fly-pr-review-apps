9a061fd4f6db40f343ef4d4ae2c33ee9f2dd4696
Damiano Petrungaro <damianopetrungaro@gmail.com>
2023-08-08 18:28:31 +0200

    fix: apply secrets and attach postgres

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,11 +18,17 @@ REPO_NAME=$(jq -r .event.base.repo.name /github/workflow/event.json)
 EVENT_TYPE=$(jq -r .action /github/workflow/event.json)
 
 # Default the Fly app name to pr-{number}-{repo_owner}-{repo_name}
-app="${INPUT_NAME:-pr-$PR_NUMBER-$REPO_OWNER-$REPO_NAME}"
+app="${INPUT_APP:-${FLY_APP:-pr-$PR_NUMBER-$REPO_OWNER-$REPO_NAME}}"
 region="${INPUT_REGION:-${FLY_REGION:-iad}}"
 org="${INPUT_ORG:-${FLY_ORG:-personal}}"
 image="$INPUT_IMAGE"
 config="$INPUT_CONFIG"
+vm="${INPUT_VM:-shared-cpu-1x}"
+ha="${INPUT_HA:-false}"
+
+# Prepare the args
+args=$(echo "$INPUT_ARGS" | sed '/^[[:space:]]*$/d' | sed 's/\(.*\)/ --build-arg \1 / ' | sed 's/^ *//')
+args=$(echo "$args" | tr -d '\n')
 
 if ! echo "$app" | grep "$PR_NUMBER"; then
   echo "For safety, this action requires the app's name to contain the PR number."
@@ -35,26 +41,40 @@ if [ "$EVENT_TYPE" = "closed" ]; then
   exit 0
 fi
 
-# Deploy the Fly app, creating it first if needed.
-if ! flyctl status --app "$app"; then
-  flyctl launch --no-deploy --copy-config --name "$app" --image "$image" --region "$region" --org "$org"
-  if [ -n "$INPUT_SECRETS" ]; then
-    echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$app"
+# Ensure deployment is triggered if app is not available
+if [ "$INPUT_UPDATE" = "false" ]; then
+  if ! flyctl status --app "$app"; then
+    INPUT_UPDATE="true"
   fi
-  flyctl deploy --app "$app" --region "$region" --image "$image" --region "$region" --strategy immediate
-elif [ "$INPUT_UPDATE" != "false" ]; then
-  flyctl deploy --config "$config" --app "$app" --region "$region" --image "$image" --region "$region" --strategy immediate
+fi
+
+# Create Fly App
+if ! flyctl status --app "$app"; then
+  cp "$config" "$config.backup"
+  flyctl launch --no-deploy --copy-config --name "$app" --image "$image" --regions "$region" --org "$org" --ha="$ha" --vm-size="$vm" $args
+  cp "$config.backup" "$config"
+fi
+
+# Import secrets
+if [ -n "$INPUT_SECRETS" ]; then
+  echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$app"
 fi
 
 # Attach postgres cluster to the app if specified.
 if [ -n "$INPUT_POSTGRES" ]; then
-  flyctl postgres attach --postgres-app "$INPUT_POSTGRES" || true
+  flyctl postgres attach -a $app -y "$INPUT_POSTGRES" || true
+fi
+
+# Deploy the Fly App
+if [ "$INPUT_UPDATE" != "false" ]; then
+  flyctl deploy --config "$config" --app "$app" --image "$image" --regions "$region" --strategy immediate --ha="$ha" --vm-size="$vm" $args
 fi
 
 # Make some info available to the GitHub workflow.
 fly status --app "$app" --json >status.json
 hostname=$(jq -r .Hostname status.json)
 appid=$(jq -r .ID status.json)
-echo "::set-output name=hostname::$hostname"
-echo "::set-output name=url::https://$hostname"
-echo "::set-output name=id::$appid"
+echo "app=$app" >> $GITHUB_OUTPUT
+echo "hostname=$hostname" >> $GITHUB_OUTPUT
+echo "url=https://$hostname" >> $GITHUB_OUTPUT
+echo "id=$appid" >> $GITHUB_OUTPUT
